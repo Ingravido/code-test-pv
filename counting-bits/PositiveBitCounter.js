2bdddc13a66b418bddaa60a9cfe08a8fa6e44266
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-06-29 23:08:49 +0200

    feat(counting-bits): First version using forEach. Avoiding Arr.reverse()

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,32 @@
 function Count (input) {
-  throw new Error('Not implemented')
+  if (input < 0) {
+    throw new RangeError()
+  }
+
+  let onesSum = 0
+  const oneBitFoundList = []
+  const binaryRepresentation = intToBinaryString(input);
+
+  [...binaryRepresentation].forEach(getSumAndOnesPositions)
+
+  function getSumAndOnesPositions (bitString, position) {
+    const bitNumber = Number(bitString)
+    onesSum += bitNumber
+    storeIfOneInReversedOrder(bitNumber, position)
+  }
+
+  function storeIfOneInReversedOrder (bitNumber, position) {
+    if (bitNumber === 1) {
+      const reversedPosition = (binaryRepresentation.length - 1) - position
+      oneBitFoundList.unshift(reversedPosition)
+    }
+  }
+
+  return [onesSum, ...oneBitFoundList]
+}
+
+function intToBinaryString (input) {
+  return input.toString(2)
 }
 
 module.exports = { Count }
