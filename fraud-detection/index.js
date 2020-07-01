3dda3edf7b16ed194aae039d50ccd2da71791899
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-07-02 01:23:59 +0200

    refactor(fraud-detection): just a nicer name to fraud check engine

--- fraud-detection/index.js
+++ fraud-detection/index.js
@@ -1,44 +1,12 @@
 const normalizer = require('./components/normalizer')
 const ordersParser = require('./components/orders-parser')
+const fraudChecker = require('./components/fraud-check-engine')
 
 function Check (filePath) {
-  // READ FRAUD LINES
   const orders = ordersParser.parseOrders(filePath)
   normalizer.normalize(orders)
-  let fraudResults = []
 
-  // CHECK FRAUD //TODO: EXTRACT TO COMPONENT
-  for (let i = 0; i < orders.length; i++) {
-    let current = orders[i]
-    let isFraudulent = false
-
-    for (let j = i + 1; j < orders.length; j++) { // forEach
-      isFraudulent = false
-      if (current.dealId === orders[j].dealId &&
-        current.email === orders[j].email &&
-        current.creditCard !== orders[j].creditCard) {
-        isFraudulent = true
-      }
-
-      if (current.dealId === orders[j].dealId &&
-        current.state === orders[j].state &&
-        current.zipCode === orders[j].zipCode &&
-        current.street === orders[j].street &&
-        current.city === orders[j].city &&
-        current.creditCard !== orders[j].creditCard) {
-        isFraudulent = true
-      }
-
-      if (isFraudulent) {
-        fraudResults.push({
-          isFraudulent: true,
-          orderId: orders[j].orderId
-        })
-      }
-    }
-  }
-
-  return fraudResults
+  return fraudChecker.findFraudulentOrders(orders)
 }
 
 module.exports = { Check }
