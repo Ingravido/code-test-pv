2793068abd9b7bbf640cacf4765b3e14f6b2d67c
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-07-02 01:25:06 +0200

    refactor(fraud-detection): refactored orders traversing and fraud check. Now complexity is O(n)

---
+++ fraud-detection/components/fraud-check-engine/index.js
@@ -0,0 +1,41 @@
+function findFraudulentOrders (orders) {
+  let fraudResults = []
+  const ordersMap = new Map()
+
+  orders.forEach(processOrder)
+
+  function processOrder (order) {
+    const emailKey = `${order.dealId}${order.email}`
+    const streetKey = `${order.dealId}${order.state}${order.zipCode}${order.street}${order.city}`
+
+    let isFraudulent = false
+    triggerFraudFlagOrStoreOrder(emailKey)
+    triggerFraudFlagOrStoreOrder(streetKey)
+
+    if (isFraudulent) {
+      markAndStoreFraudulentOrder(order)
+    }
+
+    function triggerFraudFlagOrStoreOrder (key) {
+      const fraudCheckByEmail = ordersMap.has(key) &&
+        ordersMap.get(key).creditCard !== order.creditCard
+
+      if (fraudCheckByEmail) {
+        isFraudulent = true
+      } else {
+        ordersMap.set(key, order)
+      }
+    }
+
+    function markAndStoreFraudulentOrder (order) {
+      fraudResults.push({
+        isFraudulent: true,
+        orderId: order.orderId
+      })
+    }
+  }
+
+  return fraudResults
+}
+
+module.exports = { findFraudulentOrders }
