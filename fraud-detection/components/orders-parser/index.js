412cbb7c81a4bd01c7cab9a33d25e1565d5fef13
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-07-02 02:30:22 +0200

    refactor(fraud-detection): now destructuring order fields from line.split

--- fraud-detection/components/orders-parser/index.js
+++ fraud-detection/components/orders-parser/index.js
@@ -4,19 +4,19 @@ function parseOrdersFromCSVLines (lines) {
   const orders = []
 
   for (let line of lines) {
-    // destruc items ?
-    // enforce items[x]
-    let items = line.split(',')
-    let order = {
-      orderId: Number(items[0]),
-      dealId: Number(items[1]),
-      email: items[2].toLowerCase(),
-      street: items[3].toLowerCase(),
-      city: items[4].toLowerCase(),
-      state: items[5].toLowerCase(),
-      zipCode: items[6],
-      creditCard: items[7]
+    const [orderId, dealId, email, street, city, state, zipCode, creditCard] = line.split(',')
+
+    const order = {
+      orderId: Number(orderId),
+      dealId: Number(dealId),
+      email: email.toLowerCase(),
+      street: street.toLowerCase(),
+      city: city.toLowerCase(),
+      state: state.toLowerCase(),
+      zipCode: zipCode,
+      creditCard
     }
+
     orders.push(order)
   }
 
