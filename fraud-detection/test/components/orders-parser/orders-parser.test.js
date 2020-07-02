361bd397d89df117f530fcdc11ace58488a79961
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-07-02 17:57:57 +0200

    test(fraud-detection): using in test dependency inyection. Using sinon to mock file reader

--- fraud-detection/test/components/orders-parser/orders-parser.test.js
+++ fraud-detection/test/components/orders-parser/orders-parser.test.js
@@ -1,9 +1,26 @@
+const sinon = require('sinon')
 const ordersParser = require('../../../components/orders-parser')
 const assert = require('assert')
 
+const incompleteOrdersOneGood = [
+  '1,1,weird,stuff,,',
+  '2,2,rare,order',
+  ',,,,,,,,,,,,',
+  '',
+  '4,1,bugs@bunny.com,123 Sesame St.,New York,NY,10011,12345689010'
+]
+
 describe('orders-parser', () => {
   it('should protect order parsing ignoring incomplete or weird lines', async () => {
-    const result = await ordersParser.parseOrders('test/fixtures/SomeWeirdOrdersAndOneGood.txt')
+    // Given
+    const readFileLinesFromFilePath = sinon.stub().returns(incompleteOrdersOneGood)
+    const fileReaderMock = {readFileLinesFromFilePath}
+    ordersParser.init({fileReader: fileReaderMock})
+
+    // When
+    const result = await ordersParser.parseOrders()
+
+    // Then
     assert.ok(result)
     assert.equal(result.length, 1)
   })
